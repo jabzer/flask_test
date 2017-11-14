83f282febc410fbdcd40fa8efc954e6f461dd10a
jabzer <maihongjieer@qq.com>
2017-11-14 11:02:08 +0800

    运行db连接数据库，看看有没有报错

--- flask_jichu/db_test.py
+++ flask_jichu/db_test.py
@@ -4,9 +4,11 @@ from flask import Flask,url_for,redirect,render_template
 from flask_sqlalchemy import SQLAlchemy
 import config
 
-app =  Flask(__name__)
+app = Flask(__name__)
 app.config.from_object(config)
 db = SQLAlchemy(app)
+
+db.create_all()
 
 @app.route('/')
 def index():
