73451dba1889fbc1d54a9e997734c919b49c01b0
jabzer <maihongjieer@qq.com>
2017-11-14 12:24:24 +0800

    外键的约束

--- flask_jichu/db_test.py
+++ flask_jichu/db_test.py
@@ -8,11 +8,20 @@ app = Flask(__name__)
 app.config.from_object(config)
 db = SQLAlchemy(app)
 
+#用户表
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer,primary_key=True,autoincrement=True)
+    username = db.Column(db.String(100),nullable=False)
+
+#文章表
 class Article(db.Model):
     __tablename__ = 'article'
     id = db.Column(db.Integer,primary_key=True,autoincrement=True)
     title = db.Column(db.String(100),nullable=False)
     content = db.Column(db.Text,nullable=False)
+    author_id = db.Column(db.Integer,db.ForeignKey('user.id'))
+    author = db.relationship('User',backref=db.backref('articles'))#引用和反向引用
 
 db.create_all()
 
@@ -33,10 +42,29 @@ def index():
     # article1.content += '，没有下雨'
     # db.session.commit()
 
-    #删
-    article1 = Article.query.filter(Article.id == 1).first()
-    db.session.delete(article1)
-    db.session.commit()
+    # #删
+    # article1 = Article.query.filter(Article.id == 1).first()
+    # db.session.delete(article1)
+    # db.session.commit()
+
+    # #添加一个用户
+    # user1 = User(username = 'admin')
+    # db.session.add(user1)
+    # db.session.commit()
+    #
+    # #添加一篇文章
+    # article1 = Article(title='111',content='222',author_id='1')
+    # db.session.add(article1)
+    # db.session.commit()
+
+    #通过文章找到作者
+    article1 = Article.query.filter(Article.title == 'aaa').first()
+    print(article1.author.username)
+    #通过作者找到文章
+    user = User.query.filter(User.username == 'admin').first()
+    result = user.articles
+    for article  in result:
+        print(article.title,article.content)
 
     return 'hello'
 
