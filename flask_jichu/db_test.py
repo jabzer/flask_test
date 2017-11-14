54a6586bc24d5627e64c790e3e2795e38b0f74aa
jabzer <maihongjieer@qq.com>
2017-11-14 11:41:05 +0800

    数据的增删改查

--- flask_jichu/db_test.py
+++ flask_jichu/db_test.py
@@ -13,11 +13,31 @@ class Article(db.Model):
     id = db.Column(db.Integer,primary_key=True,autoincrement=True)
     title = db.Column(db.String(100),nullable=False)
     content = db.Column(db.Text,nullable=False)
-    
+
 db.create_all()
 
 @app.route('/')
 def index():
+    # #增加：
+    # article1 = Article(title='11月的第一天',content='天气晴朗')
+    # db.session.add(article1)
+    # #事务
+    # db.session.commit()
+
+    # #查
+    # article1 = Article.query.filter(Article.title == '11月的第一天').first()
+    # print(article1.title,article1.content)
+
+    # #改
+    # article1 = Article.query.filter(Article.title == '11月的第一天').first()
+    # article1.content += '，没有下雨'
+    # db.session.commit()
+
+    #删
+    article1 = Article.query.filter(Article.id == 1).first()
+    db.session.delete(article1)
+    db.session.commit()
+
     return 'hello'
 
 if __name__ == '__main__':
